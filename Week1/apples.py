bf51d9d47f27a860455c263e3dfc311012d656d9
Luis.tamayo <luis.tamayo@bryanuniversity.edu>
2023-07-21 13:26:59 -0600

    Week1

---
+++ Week1/apples.py
@@ -0,0 +1,6 @@
+apple_price = 0.50
+name = input('Please enter your name:')
+print(f"Hi {name}. Apples cost ${apple_price} each. How many would you like to buy?")
+qty = int(input())
+print(f"Thank you {name} for your purchase of {qty} apples at a cost of {apple_price}  each")
+
---
+++ Week1/hello.py
@@ -0,0 +1,5 @@
+from cgi import print_arguments
+
+
+msg = "Hello World"
+print(msg)
