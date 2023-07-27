badea621839dfa2648cb5b6a0db2fffaffc2ce46
Luis.tamayo <luis.tamayo@bryanuniversity.edu>
2023-07-27 12:47:45 -0600

    magic.py

---
+++ Week2/magic.py
@@ -0,0 +1,9 @@
+number = int(input("Please enter a number:"))
+a = 3
+b = 6
+mult = (number * a)
+add = (mult + b)
+div = (add // a)
+magic = (div - number)
+print(f"Your Number:{number}")
+print(f"Tada: {magic}")
