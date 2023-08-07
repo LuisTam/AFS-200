6c62f2c1ed98e8e8a26afa79700d7a7a5705931d
Luis.tamayo <luis.tamayo@bryanuniversity.edu>
2023-08-06 18:56:14 -0600

    onlyeven.py

--- Week3/onlyeven.py
+++ Week3/onlyeven.py
@@ -1,12 +1,12 @@
 def userNumber():
     while True:
         #Asking user for input
-        userInput = input("Please enter positive number: ")
+        userInput = input("Please enter a positive number: ")
         #Checks to see if input is a number, if so it stores it as an interger
         if userInput.isdigit():
             number = int(userInput)
             #If input is Postive number is calculates and prints even numbers
-            evens = [number for number in range(number) if number % 2 == 0]
+            evens = [number for number in range(number+1) if number % 2 == 0]
             print(evens)
             #Checks to see if it is a positive number
             if number > 0:
